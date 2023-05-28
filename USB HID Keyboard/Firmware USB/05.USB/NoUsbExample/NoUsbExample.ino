b9e0ef578b668010e20e905ce10e81e144416e0c
NVDGit <k3751070033@gmail.com>
2023-05-28 16:24:25 +0700

    First commit

---
+++ USB HID Keyboard/Firmware USB/05.USB/NoUsbExample/NoUsbExample.ino
@@ -0,0 +1,29 @@
+/*
+  No USB example
+
+
+  created 2021
+  by Deqing Sun for use with CH55xduino
+
+  This example code is in the public domain.
+
+  cli board options: usb_settings=user0
+
+*/
+
+#ifndef USER_USB_RAM
+#error "This example needs to be compiled with a USER USB setting"
+#endif
+
+void USBInterrupt(void){ 
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
